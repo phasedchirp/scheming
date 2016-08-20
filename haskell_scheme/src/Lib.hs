6d7c87eb69ed5b452c63565150031f264b5a833e
phasedchirp <sean.martin@nyu.edu>
2016-08-20 17:10:11 -0400

    Worked on input parsing

--- haskell_scheme/src/Lib.hs
+++ haskell_scheme/src/Lib.hs
@@ -1,7 +1,21 @@
 module Lib where
 
+import Control.Monad (liftM)
+import Numeric (readHex,readOct,readInt)
+import Data.Char (digitToInt)
 import Text.Megaparsec hiding (spaces,space,lexeme)
 import Text.Megaparsec.String
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | Character Char
+             | String String
+             | Bool Bool
+             deriving (Show)
+
+--  Parsers:
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -10,22 +24,18 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipSome spaceChar
 
-readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-  Left err -> "No match" ++ show err
-  Right val -> "Found value"
+escapes = [('b','\b'),('n','\n'),('f','\f'),('r','\r'),('t','\t'),('\\','\\'),('"','"')]
 
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | String String
-             | Bool Bool
+escapes' = map (\(x, y) -> char x >> return y) escapes
+
+escapeChar :: Parser Char
+escapeChar = char '\\' >> choice escapes'
+
 
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many (noneOf "\"")
+  x <- many $ (escapeChar <|> noneOf "\"")
   char '"'
   return $ String x
 
@@ -38,3 +48,45 @@ parseAtom = do
     "#t" -> Bool True
     "#f" -> Bool False
     _ -> Atom atom
+
+
+parseDecimal :: Parser LispVal
+parseDecimal = some digitChar >>= \x -> return $ (Number . read) x
+
+readOct' = fst . head . readOct
+parseOctal :: Parser LispVal
+parseOctal = some octDigitChar >>= \x -> return $ (Number . readOct') x
+
+readHex' = fst . head . readHex
+parseHex :: Parser LispVal
+parseHex = some hexDigitChar >>= \x -> return $ (Number . readHex') x
+
+-- Probably a better way to do this?
+readBin = readInt 2 (`elem` "01") digitToInt
+readBin' = fst . head . readBin
+parseBin :: Parser LispVal
+parseBin = some digitChar >>= \x -> return $ (Number . readBin') x
+
+parseBase :: Parser LispVal
+parseBase = do
+  radix <- char '#' >> letterChar
+  case radix of 'd' -> parseDecimal
+                'o' -> parseOctal
+                'x' -> parseHex
+                'b' -> parseBin
+
+parseNumber :: Parser LispVal
+parseNumber = parseDecimal <|> parseBase
+
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+parseExpr' :: Parser LispVal
+parseExpr' = spaces >> parseExpr
+
+-- Read input:
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match" ++ show err
+  Right _ -> "Found value"
