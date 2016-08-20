14ced61752c42ba2cc681b608cf3b0e40e98e749
phasedchirp <sean.martin@nyu.edu>
2016-08-20 18:15:36 -0400

    Added basic LispVal type to main.rs and added nom as a dependency in Cargo.toml

--- rust_scheme/src/main.rs
+++ rust_scheme/src/main.rs
@@ -1,6 +1,22 @@
+extern crate nom;
+
 use std::env;
+
+// feels excessively haskell-y maybe?
+#[derive(Clone, Debug, PartialEq)]
+enum LispVal {
+    Atom(String),
+    List(Vec<LispVal>),
+    DottedList(Vec<LispVal>,Box<LispVal>),
+    Number(u64),
+    LString(String),
+    LBool(bool),
+    LChar(char)
+}
 
 fn main() {
     let arg_vec: Vec<String> = env::args().collect();
-    println!("Hello, {}!",arg_vec[1]);
+    let test = LispVal::List(vec![LispVal::Number(64),LispVal::Number(24)]);
+    println!("Hello, {:?}!",test);
+    println!("{}", arg_vec[1]);
 }
