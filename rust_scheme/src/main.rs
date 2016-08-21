860ff828ac788dbbf509d55d5dcffdfbfd5aa39a
phasedchirp <sean.martin@nyu.edu>
2016-08-21 15:48:57 -0400

    Added parser stubs to rust version, some experimenting with nom in main.

--- rust_scheme/src/main.rs
+++ rust_scheme/src/main.rs
@@ -1,13 +1,17 @@
 #[macro_use]
 extern crate nom;
+extern crate num;
 
 use std::env;
 mod scheme;
 use scheme::*;
+use nom::*;
+
+named!(test_parser,tag!("#\\"));
 
 fn main() {
     let arg_vec: Vec<String> = env::args().collect();
-    // let test = parse_bool("#tstuff");
-    // println!("{:?}",test);
+    let test = test_parser(b"#\\axyz");
+    println!("{:?}",test);
     println!("Hello, {}!", arg_vec[1]);
 }
--- rust_scheme/src/scheme.rs
+++ rust_scheme/src/scheme.rs
@@ -1,17 +1,44 @@
 // extern crate nom;
-
+use num::Complex;
 use nom::*;
 
 // Data types:
 #[derive(Clone, Debug, PartialEq)]
 pub enum LispVal {
     Atom(String),
-    List(Vec<LispVal>),
-    DottedList(Vec<LispVal>,Box<LispVal>),
+    List(Vec<LispVal>), // Possibly should be singly-linked list instead?
+    DottedList(Vec<LispVal>,Box<LispVal>), // not quite proper definition.
+    Vector(Vec<LispVal>), // Scheme standard calls for both but not different from List...
     Number(u64),
+    Rational(u64,u64),
+    Float(f64),
+    Complex(Complex<f64>),
     LString(String),
     LBool(bool),
     LChar(char)
 }
 
-named!(parse_bool,alt!(tag!("#t") | tag!("#f")));
+// Parsers:
+// named!(parse_symbol,one_of!("!$%&|*+-/:<=>?@^_~"));
+// named!(parse_bool,alt!(tag!("#t") | tag!("#f")));
+// named!(parse_atom,unimplemented!());
+// named!(parse_list,unimplemented!());
+// named!(parse_vector,unimplemented!());
+// named!(parse_dottedlist,unimplemented!());
+// named!(parse_string,unimplemented!());
+// named!(parse_quoted,unimplemented!());
+// named!(parse_quasiquote,unimplemented!());
+// named!(parse_unquote,unimplemented!());
+//
+// // parsing numeric values:
+// named!(parse_number,unimplemented!());
+// named!(parse_rational,unimplemented!());
+// named!(parse_decimal,unimplemented!());
+// named!(parse_hex,unimplemented!());
+// named!(parse_octal,unimplemented!());
+// named!(parse_binary,unimplemented!());
+// named!(parse_complex,unimplemented!());
+// named!(parse_base,unimplemented!());
+//
+// // Sticking everything together:
+// named!(parse_expr,unimplemented!());
