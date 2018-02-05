168094ecd94cb219700ce3d2e68b656937d46bec
phasedchirp <phasedchirp2718@gmail.com>
2018-02-04 16:30:27 -0800

    *Slightly* less clunky tokenizing

--- rust_version/src/main.rs
+++ rust_version/src/main.rs
@@ -1,8 +1,15 @@
+// following pointers from http://norvig.com/lispy.html
 use std::io::{stdout, stdin, Write};
 use std::collections::HashSet;
 
-fn is_op(c: &char, tbl: &HashSet<char>) -> bool {
+// special characters
+fn is_id(c: &char, tbl: &HashSet<char>) -> bool {
     tbl.contains(c)
+}
+
+// whitespace
+fn is_ws(c: &char) -> bool {
+    c == &' ' || c == &'\n' || c == &'\t'
 }
 
 #[derive(Debug, PartialEq)]
@@ -31,45 +38,52 @@ impl Tokens {
     }
 }
 
-fn tokenize(s: &str) -> Tokens {
+fn tokenize(s: &str, ids: &HashSet<char>) -> Tokens {
     let mut tokens = Tokens::new();
-    let mut input = s.chars().peekable();
-    loop {
-        match input.peek() {
-            Some(&'(') => tokens.push(
-                Atom(input.next().unwrap().to_string())
-            ),
-            Some(&')') =>  tokens.push(
-                Atom(input.next().unwrap().to_string())
-            ),
-            Some(_)    => {
-                let mut token = String::new();
-                while input.peek() != Some(&')')
-                   && input.peek() != Some(&'(') {
-                       token.push(input.next().unwrap());
-                   }
-                tokens.push(Atom(token));
-            },
-            None => return tokens
-
+    let mut token = String::new();
+    for c in s.chars() {
+        if is_id(&c,ids) {
+            if !token.is_empty() {
+                tokens.push(Atom(token.clone()));
+                token = String::new();
+            }
+            tokens.push(Atom(c.to_string()));
+        } else if is_ws(&c) {
+            if !token.is_empty() {
+                tokens.push(Atom(token.clone()));
+                token = String::new();
+            }
+            continue;
+        } else {
+            token.push(c);
         }
     }
+    if !token.is_empty() {
+        tokens.push(Atom(token));
+    }
+    tokens
 }
 
-fn repl() {
+// fn make_ast()
+
+fn repl(ids: &HashSet<char>) {
     loop {
         let mut user_input = String::new();
         print!("> ");
         stdout().flush().expect("Something went wrong?");
         stdin().read_line(&mut user_input)
                    .expect("Failed to read input");
-        let mut tokens = tokenize(&user_input.trim());
+        let mut tokens = tokenize(&user_input.trim(),ids);
     }
 
 }
 
 fn main() {
-    let test = tokenize("(stuff(nested(inside(of(stuff)))))");
+    let mut ids = HashSet::new();
+    for id in vec!['(',')'] {
+        ids.insert(id);
+    }
+    let test = tokenize("(stuff(nested(inside(of(stuff)))))",&ids);
     println!("{:?}",test);
     // repl();
  }
